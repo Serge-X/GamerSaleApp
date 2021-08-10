e571fc78bb4c36c8bd77212e37fcbc97360d0134
Serge <lionheart.sergex@gmail.com>
2021-08-09 19:25:21 -0700

    Tuned code for input form

--- gamersalez/src/Components/Searchbar.js
+++ gamersalez/src/Components/Searchbar.js
@@ -1,13 +1,18 @@
 import  React from 'react'
-import { Button, FormControl } from 'react-bootstrap'
+// import { Button, FormControl } from 'react-bootstrap'
+import FormControl from 'react-bootstrap/FormControl'
 import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
+import { FormLabel } from 'react-bootstrap'
 
 export const Searchbar = () => {
     return (
-        <div id="searchBar">
-            <InputGroup>
+        <div  >
+            <InputGroup size="sm" id="putGroup"  >
                 <FormControl 
+                    className="bar"
                     placeholder="Search Games"
+                    aria-label="Search Games"
                 />
                 <Button >
                     Search
