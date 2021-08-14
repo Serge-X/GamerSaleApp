5327d6853a4efee3b5e945c44b8150a71f639f27
Serge <lionheart.sergex@gmail.com>
2021-08-14 15:51:12 -0700

    Refined search bar

--- gamersalez/src/Components/Searchbar.js
+++ gamersalez/src/Components/Searchbar.js
@@ -3,18 +3,17 @@ import  React from 'react'
 import FormControl from 'react-bootstrap/FormControl'
 import InputGroup from 'react-bootstrap/InputGroup'
 import Button from 'react-bootstrap/Button'
-import { FormLabel } from 'react-bootstrap'
 
 export const Searchbar = () => {
     return (
         <div  >
             <InputGroup size="sm" id="putGroup"  >
                 <FormControl 
-                    className="bar"
+                    id="bar"
                     placeholder="Search Games"
                     aria-label="Search Games"
                 />
-                <Button >
+                <Button id="searchButton">
                     Search
                 </Button>
             </InputGroup>
